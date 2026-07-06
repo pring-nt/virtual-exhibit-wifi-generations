c0f20cca2e43e5048f29abf299c18ddbb9b69c1b
nathan <natetrndd@gmail.com>
2026-07-07 00:29:48 +0800

    fix: I am genuinely the worst developer of all time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,9 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   integrations: [mdx(), react(), icon()],
   site: 'https://pring-nt.github.io',
